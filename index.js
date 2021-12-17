9d0e96d1a58e8ff4848d995804f768b50c370557
Malik Breitbach <malikbreitbach@bsnstudent.nl>
2021-12-17 10:49:59 +0100

    we added a countdown to the date (21st may)

---
+++ index.js
@@ -0,0 +1,28 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+
+
+
+
+const munTime = new Date("May 21 2022 00:00:00");
+
+function updateCountdowntime(){
+    const currentTime = new Date();
+    const diff = munTime - currentTime;
+
+    const d = Math.floor(diff / 1000 / 60 /60 / 24);
+    const h = Math.floor(diff / 1000 / 60 /60 % 24)
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000 ) % 60;
+
+    days.innerHTML= d < 10 ? '0' + d:d;
+    hours.innerHTML = h < 10 ? '0' + h:h;
+    minutes.innerHTML = m < 10 ? '0' + m:m;
+    seconds.innerHTML = s < 10 ? '0' + s:s;
+}
+
+setInterval(updateCountdowntime, 1000);
+
